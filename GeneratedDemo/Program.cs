36aa93e2ff1905234fcb627b4bc54bfbc4a7a7c1
JejuniProgramming <jejuniprogramming@gmail.com>
2021-07-16 00:02:55 +0200

    Add project files.

---
+++ GeneratedDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GeneratedDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vm = new ExampleViewModel();
+            UseAutoNotifyGenerator.Run();
+        }
+    }
+}
